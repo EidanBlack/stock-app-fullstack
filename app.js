ea333c51ae92362153c46f7c64d177441f461dfe
adrian candia <eidanblack19@gmail.com>
2022-06-06 23:13:42 -0400

    carga de producto a bd

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 mongoose
-.connect(`mongodb+srv://Divague190594:${process.env.MONGO_DB_PASS}@development.d2u5n.mongodb.net/?retryWrites=true&w=majority`)
+.connect(`mongodb+srv://Divague190594:${process.env.MONGO_DB_PASS}@development.d2u5n.mongodb.net/stock-app?retryWrites=true&w=majority`)
 .then((result) => {
     console.log("Conexion a BBDD exitosa!");
     app.listen(PORT, ()=>{
@@ -16,13 +16,30 @@ mongoose
 })
 .catch((err) => console.log(err));
 
+const productSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    price: Number,
+    },
+    {timestamps: true}
+);
+
+const Product = mongoose.model('Product', productSchema, 'Products');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 
-app.post('/api/v1/products', (req, res, next) =>{
-    console.log({body: req.body});
-    res.status(201).json({ok: true});
+app.post('/api/v1/products', async (req, res, next) =>{
+    const newProduct = new Product({
+        name: req.body.name,
+        price: req.body.price,
+    });
+
+    await newProduct.save().then((result)=>{
+        res.status(201).json({ok: true});
+
+    })
+    .catch((error) => console.log(error));
 });
 
 const PORT = process.env.PORT || 4000;
