ecdcdf04afb9535f65ff4e4ef3a925eb2d1a1212
adrian candia <eidanblack19@gmail.com>
2022-06-06 21:49:46 -0400

    pintamos el index.html

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) =>{
     console.log("Petición recibida");
-
-    res.status(200).sendFile(index.html);
 });
 
 const PORT = process.env.PORT || 4000;
